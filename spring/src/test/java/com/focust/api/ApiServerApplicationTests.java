83e147d1c0464fd391dbe7bdb7c67e004fcfc3bc
Allan DeBoe <allan.m.deboe@gmail.com>
2024-10-02 14:44:59 -0700

    Created Base Spring Application
    Created a base Spring application from Spring Initializr, added "JGit" as a dependency, modified the `application.properties` file to include relevant app information, added GNU license information onto the generated `.java` files, and created a `Dockerfile` as well as a `.dockerignore` file.

---
+++ spring/src/test/java/com/focust/api/ApiServerApplicationTests.java
@@ -0,0 +1,44 @@
+/**
+ * ApiServerApplicationTests.java - Tests for the Spring Application
+ * Copyright (C) 2024  Allan DeBoe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ------------------------------------------------------------------------
+ *
+ * @author Allan DeBoe (allan.m.deboe@gmail.com)
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+package com.focust.api;
+
+///////////////////////////////////////////////////////////////////////////
+
+// JUnit //
+import org.junit.jupiter.api.Test;
+
+// Spring Framework //
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+///////////////////////////////////////////////////////////////////////////
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring/src/test/java/com/focust/api/TestcontainersConfiguration.java
@@ -0,0 +1,47 @@
+/**
+ * TestcontainersConfiguration.java - Configuration for MySQL Testcontainer
+ * Copyright (C) 2024  Allan DeBoe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * ------------------------------------------------------------------------
+ *
+ * @author Allan DeBoe (allan.m.deboe@gmail.com)
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+package com.focust.api;
+
+///////////////////////////////////////////////////////////////////////////
+
+// Spring Framework //
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+
+// Testcontainers //
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+///////////////////////////////////////////////////////////////////////////
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+	@Bean
+	@ServiceConnection
+	MySQLContainer<?> mysqlContainer() {
+		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+	}
+
+}
