989ea0cd2302ec13fd6977d32236581140c5ed3f
Allan DeBoe <allan.m.deboe@gmail.com>
2024-10-24 01:14:11 -0700

    Added Jenkinsfile
    For this commit I added the `Jenkinsfile` that will be tested to see if Jenkins is able to properly build the back-end and database servers. I also modified the `run.sh` file to allow me to run the back-end server with and without tests.

--- spring/run.sh
+++ spring/run.sh
@@ -1,3 +1,24 @@
-mvn -q clean 
-mvn test
+#!/bin/bash
+
+DEV_MODE=0
+
+for arg in "$@"; do
+    case $arg in
+        -d|--dev)
+            DEV_MODE=1
+            shift
+            ;;
+        -*|--*)
+            echo "Unknown option $arg"
+            exit 1
+            ;;
+        *)
+            ;;
+    esac
+done
+
+mvn -q clean
+if [[ $DEV_MODE -eq 1 ]]; then
+    mvn test
+fi
 mvn -q spring-boot:run
